e4a7b7a0910930490e995e305b9fb097931f5d7e
saifkenani-SW <saifkhaledkenani@gmail.com>
2026-07-28 23:22:35 +0300

    chore(cors): update allowed origin to http://localhost:4200

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   // 1. أمن أساسي
 
   app.enableCors({
-    origin: ['https://localhost:4200'],
+    origin: ['http://localhost:4200', 'http://localhost:4000'],
     credentials: true,
   });
   app.use(helmet());
